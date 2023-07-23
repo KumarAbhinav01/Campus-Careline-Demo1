2758aae3461339b4ed044d17bf3534978f625c16
KumarAbhinav01 <abhinav.bitd@gmail.com>
2023-07-23 20:19:14 +0530

    Config File Update

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
-import dotenv from 'dotenv';
-
-// Load environment variables from the .env file
-dotenv.config();
-
 const config = {
-    baseURL: process.env.BASE_URL | 'https://campus-careline-demo1.onrender.com',   
-};
+    baseURL: "https://campus-careline-demo1.onrender.com",
+    
+  };
   
 export default config;
   
